9cecdc9c8956e382357d937f0a6adf2c3db01360
Dawoon Jung <tyranno2020@gmail.com>
2025-10-08 00:07:11 +0900

    chore: bump version code to 3 for Play Console resubmission

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,7 +32,7 @@ android {
         applicationId = "com.anam145.wallet"
         minSdk = 24
         targetSdk = 35
-        versionCode = 2
+        versionCode = 3
         versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
